62e6fa8ec7cccfdd324f21b4c65741b32a32f54d
SerhioAndros <androsiuksv@gmail.com>
2021-06-19 10:26:51 +0300

    initial basic settings

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Component } from "react";
+
+class App extends Component {
+  state = {};
+  render() {
+    return <h1>Hello world!</h1>;
+  }
+}
+
+export default App;
